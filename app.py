018163949d7d6cdb2eda7e882549c2fe0fdc4dc4
Protyusha <protyusha31@gmail.com>
2024-08-26 20:46:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import uuid
+from flask import Flask, request
+from db import items,stores
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+@app.get("/store")
+def get_stores():
+    return {"stores_name": list(stores.values())}
+
+
+@app.post("/store")
+def create_store():
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
+
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+
+    return item
+
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        return {"message": "Store not found"}, 404
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        return {"message": "Item not found"}, 404
+
+if __name__ == '__main__':
+    # Enable debug mode and set the port to 5050
+    app.run(debug=True,port=6060)
